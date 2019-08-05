b39537edf9e9c9094499bf6f4bc6600496592762
Colton Kayser <coltonkayser@gmail.com>
2019-08-05 13:32:38 -0400

    styling changes

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -13,13 +13,13 @@ sessions.post('/', (req, res) => {
       console.log(err);
       res.send('db has issue');
     } else if (!foundUser) {
-      res.send('<a href="/">User Not Found</a>');
+      res.render('error.ejs');
     } else {
       if ( bcrypt.compareSync (req.body.password, foundUser.password)) {
         req.session.currentUser = foundUser;
         res.redirect('/app');
       } else {
-        res.send('<a href="/"> Wrong Password</a>');
+        res.render('error.ejs');
       }
     }
   });
